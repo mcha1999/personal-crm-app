5af381c1c8aadb44b26b3adf2b35aba148ea0a14
mcha1999 <myoungilcha@gmail.com>
2025-09-18 08:22:01 -0700

    Guard native TCP transport loading

--- services/TcpSocketTransport.native.ts
+++ services/TcpSocketTransport.native.ts
@@ -1,3 +1,20 @@
-import * as TcpSocketTransport from 'react-native-tcp-socket';
+type TcpSocketModule = typeof import('react-native-tcp-socket');
+
+const TcpSocketTransport: TcpSocketModule | null = (() => {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    return require('react-native-tcp-socket') as TcpSocketModule;
+  } catch (error) {
+    const reason = error instanceof Error ? error.message : String(error);
+
+    if (typeof console !== 'undefined') {
+      console.warn(
+        `[TcpSocketTransport] "react-native-tcp-socket" is unavailable: ${reason}. IMAP sync will be disabled.`
+      );
+    }
+
+    return null;
+  }
+})();
 
 export default TcpSocketTransport;
